51aab1aaeb1d77e6a45a5369c7fa9025f7170067
UP240357 <up240357@alumnos.upa.edu.mx>
2025-06-06 10:55:12 -0600

    Área de un triángulo

--- Day3_Level1.py
+++ Day3_Level1.py
@@ -4,4 +4,9 @@
 age = (20)
 height = (1.70)
 
-base = int(input("dame la base :"))
+#area de un triangulo
+BASE_Triangulo = float(input("dame la base :"))
+ALTURA_Triang = float(input("dame el area :"))
+
+area = 0.5*BASE_Triangulo*ALTURA_Triang
+print(area)

8f13de2bff5190737e51e94ce4564d1994603767
UP240357 <up240357@alumnos.upa.edu.mx>
2025-06-11 20:17:29 -0600

    inicio dia 4 y primer trabajo

---
+++ Day04_level01.py
@@ -0,0 +1,9 @@
+# Day 4 lvl 1 - Strings  in Python
+# Ruiz Clemente Jorge Alberto 
+
+"""
+Concatenate the string 'Thirty', 'Days', 'Of', 'Python' to
+a single string, 'Thirty Days Of Python'.
+"""
+single_string = "Thirty "+"days "+"of "+"Python"
+print(single_string)

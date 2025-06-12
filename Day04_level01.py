0ccf43e3be2926cdee3e5af184a2306c87dd7770
UP240357 <up240357@alumnos.upa.edu.mx>
2025-06-11 21:22:16 -0600

    avance hasta el ejercicio 12

--- Day04_level01.py
+++ Day04_level01.py
@@ -2,24 +2,24 @@
 # Ruiz Clemente Jorge Alberto 
 
 """ 1
-Concatenate the string 'Thirty', 'Days', 'Of', 'Python' to
-a single string, 'Thirty Days Of Python'.
+Concatenate the string 'Thirty', 'Days', 'Of', 
+'Python' to a single string, 'Thirty Days Of Python'.
 """
 single_string = "Thirty "+"Days "+"Of "+"Python"
 print(single_string)
 
 """ 2
-Concatenate the string 'Coding', 'For' , 'All' to a single string, 
-'Coding For All'.
+Concatenate the string 'Coding', 'For' , 'All' 
+to a single string, 'Coding For All'.
 """
 single_string_2 = "Coding "+"For "+"All"
 print(single_string_2)
 
 """ 3
-Declare a variable named company and assign it to an initial value 
-"Coding For All".
+Declare a variable named company and assign it 
+to an initial value "Coding For All".
 """
-company = single_string_2
+company = "Coding For All"
 
 """ 4
 Print the variable company using print().
@@ -36,5 +36,42 @@ print(len(company))
 Change all the characters to uppercase letters 
 using upper() method.
 """
-company_upper = company.upper()
-print(company_upper)
+print(company.upper())  #hace todo mayusculas
+
+""" 7
+Change all the characters to lowercase letters 
+using lower() method.
+"""
+print(company.lower())  #hace todo minusculas
+
+""" 8
+Use capitalize(), title(), swapcase() methods to 
+format the value of the string Coding For All.
+"""
+print(company.capitalize()) #mayuscula a la primera palabra
+print(company.title())      #mayuscula cada palabra
+print(company.swapcase())   #los extremos minusculas y lo de enmedio mayusculas
+
+""" 9
+Cut(slice) out the first word of Coding For All string.
+"""
+print(company.split()[0])   #separar las palabras y
+                            #con [] seleccionamos la palabra a imprimir
+
+""" 10
+Check if Coding For All string contains a word Coding 
+using the method index, find or other methods.
+"""
+print(company.index("Coding"))  #arroja la posicion en la que se encuentra la oración
+
+""" 11
+Replace the word coding in the string 'Coding For All' to Python.
+"""
+company_python = company.replace("Python", "coding")    #remplazamos una palabra
+print(company_python)                                   #primero el cambio y luego la que cambia
+
+""" 12
+Change Python for Everyone to Python for All using the replace 
+method or other methods.
+"""
+print(company_python.replace("All", "Everyone"))

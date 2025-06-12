00643ccab2955c61d8306861ef1f39da63756483
UP240357 <up240357@alumnos.upa.edu.mx>
2025-06-11 20:31:56 -0600

    avance hasta el ejercicio 5

--- Day04_level01.py
+++ Day04_level01.py
@@ -1,9 +1,37 @@
 # Day 4 lvl 1 - Strings  in Python
 # Ruiz Clemente Jorge Alberto 
 
-"""
+""" 1
 Concatenate the string 'Thirty', 'Days', 'Of', 'Python' to
 a single string, 'Thirty Days Of Python'.
 """
-single_string = "Thirty "+"days "+"of "+"Python"
+single_string = "Thirty "+"Days "+"Of "+"Python"
 print(single_string)
+
+""" 2
+Concatenate the string 'Coding', 'For' , 'All' to a single string, 
+'Coding For All'.
+"""
+single_string_2 = "Coding "+"For "+"All"
+print(single_string_2)
+
+""" 3
+Declare a variable named company and assign it to an initial value 
+"Coding For All".
+"""
+company = single_string_2
+
+""" 4
+Print the variable company using print().
+"""
+print(company)
+
+""" 5
+Print the length of the company string using 
+len() method and print().
+"""
+print(len(company))
+
+""" 6
+
+"""

7765d75ae82bfa3962c81534f414f9b5faad8af3
UP240357 <up240357@alumnos.upa.edu.mx>
2025-06-12 21:42:42 -0600

    finalizacion del dia 4

--- Day04_level01.py
+++ Day04_level01.py
@@ -174,8 +174,8 @@ Slice out the phrase 'because because because' in the
 following sentence: 'You cannot end a sentence with because 
 because because is a conjunction'
 """
-print(single_string_4[:31]+single_string_4[48:])   #imprime desde hasta, (concatena), desde hasta
-#dejarlos vacios significa [:31] = del inicio hasta 31 y [48:] = del 48 hasta el final
+print(single_string_4[:31]+single_string_4[55:])   #imprime desde hasta, (concatena), desde hasta
+#dejarlos vacios significa [:31] = del inicio hasta 31 y [55:] = del 48 hasta el final
 
 """ 26
 Find the position of the first occurrence of the word 'because' 
@@ -189,20 +189,25 @@ Slice out the phrase 'because because because' in the following
 sentence: 'You cannot end a sentence with because because because 
 is a conjunction'
 """
-print(single_string_4[:31]+single_string_4[48:])
+print(single_string_4[:31]+single_string_4[55:])
 
 """ 28
 Does ''Coding For All' start with a substring Coding?
 """
+print(company.startswith("Coding"))
 
 """ 29
 Does 'Coding For All' end with a substring coding?
 """
+print(company.endswith("coding"))
 
 """ 30
 '   Coding For All      '  , remove the left and right 
 trailing spaces in the given string.
 """
+single_string_5 = '   Coding For All      '
+print(single_string_5.strip())
+
 
 """ 31
 Which one of the following variables return True when we use the 
@@ -210,24 +215,30 @@ method isidentifier():
     30DaysOfPython
     thirty_days_of_python
 """
+print("30DaysOfPython".isidentifier())
+print("thirty_days_of_python".isidentifier())
 
 """ 32
 The following list contains the names of some of python libraries: 
 ['Django', 'Flask', 'Bottle', 'Pyramid', 'Falcon']. Join the list 
 with a hash with space string.
 """
+print(" # ".join(['Django', 'Flask', 'Bottle', 'Pyramid', 'Falcon']))
 
 """ 33
 Use the new line escape sequence to separate the following sentences.
     I am enjoying this challenge.
     I just wonder what is next.
 """
+print("I am enjoying this challenge.\nI just wonder what is next.")
 
 """ 34
 Use a tab escape sequence to write the following lines.
     Name      Age     Country   City
     Asabeneh  250     Finland   Helsinki
 """
+print("Name     \tAge   \tCountry   \tCity")
+print("Asabeneh \t250   \tFinland   \tHelsinki")
 
 """ 35
 Use the string formatting method to display the following:
@@ -235,6 +246,9 @@ Use the string formatting method to display the following:
     area = 3.14 * radius ** 2
     The area of a circle with radius 10 is 314 meters square.
 """
+radius = 10
+area = 3.14 * radius ** 2
+print(f"The area of a circle with radius {radius} is {area} meters square.")
 
 """ 36
 Make the following using string formatting methods:
@@ -246,3 +260,7 @@ Make the following using string formatting methods:
     8 // 6 = 1
     8 ** 6 = 262144
 """
+a, b = 8, 6
+print(f"{a} + {b} = {a+b}\n{a} - {b} = {a-b}\n{a} * {b} = {a*b}")
+print(f"{a} / {b} = {a/b}\n{a} % {b} = {a%b}\n{a} // {b} = {a//b}")
+print(f"{a} ** {b} = {a**b}")

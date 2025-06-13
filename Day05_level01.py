f98ddde8d07aa4d025a21ccd216e6e4349412626
UP240357 <up240357@alumnos.upa.edu.mx>
2025-06-12 22:26:47 -0600

    avance dia 5 hasta el ejer 13

--- Day05_level01.py
+++ Day05_level01.py
@@ -4,56 +4,81 @@
 """ 1
 Declare an empty list
 """ 
+empty_list = list()
+print(empty_list)
 
 """ 2
 Declare a list with more than 5 items
 """
+new_list_games = ["Fortnite", "GTA V", "Street fighter", "God of war", "Borderlands", "Fallout"]
 
 """ 3
 Find the length of your list
 """
+print(len(new_list_games))
 
 """ 4
 Get the first item, the middle item and the last item of the list
 """
+print(new_list_games[0])
+print(new_list_games[-1])
+middle = len(new_list_games)//2
+print(new_list_games[middle])
 
 """ 5
 Declare a list called mixed_data_types, put your(name, age, height, 
 marital status, address)
 """
+mixed_data_types = ["Jorge", 20, 1.69, "single", "Aguascalientes"]
 
 """ 6
 Declare a list variable named it_companies and assign initial values 
 Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon.
 """
+it_companies = ["Facebook", "Google", "Microsoft", "Apple", "IBM", "Oracle", "Amazon"]
 
 """ 7
 Print the list using print()
 """
+print(it_companies)
 
 """ 8
 Print the number of companies in the list
 """
+print(len(it_companies))
 
 """ 9
 Print the first, middle and last company
 """
+print(it_companies[0])
+print(it_companies[-1])
+middle = len(it_companies)//2
+print(it_companies[middle])
 
 """ 10
 Print the list after modifying one of the companies
 """
+it_companies[1] = "Gugle"
+print(it_companies)
 
 """ 11
 Add an IT company to it_companies
 """
+it_companies.append("Linux")
+print(it_companies)
 
 """ 12
 Insert an IT company in the middle of the companies list
 """
+middle = len(it_companies)//2
+it_companies.insert(middle,"Android")
+print(it_companies)
 
 """ 13
 Change one of the it_companies names to uppercase (IBM excluded!)
 """
+it_companies[4] = it_companies[4].upper()
+print(it_companies)
 
 """ 14
 Join the it_companies with a string '#;  '

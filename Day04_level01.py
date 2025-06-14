8a82a2e5c4f5e4cfcd7a8d22983b2aff4b07d58f
UP240357 <up240357@alumnos.upa.edu.mx>
2025-06-14 16:36:54 -0600

    mejora prints en el dia 4 lvl 1

--- Day04_level01.py
+++ Day04_level01.py
@@ -6,14 +6,14 @@ Concatenate the string 'Thirty', 'Days', 'Of',
 'Python' to a single string, 'Thirty Days Of Python'.
 """
 single_string = "Thirty "+"Days "+"Of "+"Python"
-print(single_string)
+print(f"Thirty + Days + Of + Python ={single_string}")
 
 """ 2
 Concatenate the string 'Coding', 'For' , 'All' 
 to a single string, 'Coding For All'.
 """
 single_string_2 = "Coding "+"For "+"All"
-print(single_string_2)
+print(f"\nCoding + For + All = {single_string_2}")
 
 """ 3
 Declare a variable named company and assign it 
@@ -24,88 +24,90 @@ company = "Coding For All"
 """ 4
 Print the variable company using print().
 """
-print(company)
+print(f"\nVariable company = {company}")
 
 """ 5
 Print the length of the company string using 
 len() method and print().
 """
-print(len(company))
+print(f"\nlength of company = {len(company)}")
 
 """ 6
 Change all the characters to uppercase letters 
 using upper() method.
 """
-print(company.upper())  #hace todo mayusculas
+print(f"\nuppercase of company = {company.upper()}")  #hace todo mayusculas
 
 """ 7
 Change all the characters to lowercase letters 
 using lower() method.
 """
-print(company.lower())  #hace todo minusculas
+print(f"\nlowercase of company = {company.lower()}")  #hace todo minusculas
 
 """ 8
 Use capitalize(), title(), swapcase() methods to 
 format the value of the string Coding For All.
 """
-print(company.capitalize()) #mayuscula a la primera palabra
-print(company.title())      #mayuscula cada palabra
-print(company.swapcase())   #los extremos minusculas y lo de enmedio mayusculas
+print(f"\ncapitalize of company = {company.capitalize()}") #mayuscula a la primera palabra
+print(f"\ntitle of company = {company.title()}")      #mayuscula cada palabra
+print(f"\nswapcase of company = {company.swapcase()}")   #los extremos minusculas y lo de enmedio mayusculas
 
 """ 9
 Cut(slice) out the first word of Coding For All string.
 """
-print(company.split()[0])   #separar las palabras y
-                            #con [] seleccionamos la palabra a imprimir
+#separar las palabras y
+#con [] seleccionamos la palabra a imprimir
+print(f"\ncut out the first word of coding for all = {company.split()[0]}")
 
 """ 10
 Check if Coding For All string contains a word Coding 
 using the method index, find or other methods.
 """
-print(company.index("Coding"))  #arroja la posicion en la que se encuentra la oración
+word_coding_position = company.find("Coding")
+print(f"\nposition of Coding = {word_coding_position}")  #arroja la posicion en la que se encuentra la oración
 
 """ 11
 Replace the word coding in the string 'Coding For All' to Python.
 """
 company_python = company.replace("Coding","Python")    #remplazamos una palabra
-print(company_python)                                  #primero busca y luego cambia
+print(f"\nreplace the word coding for Python = {company_python}") #primero busca y luego cambia
 
 """ 12
 Change Python for Everyone to Python for All using the replace 
 method or other methods.
 """
 company_python = company_python.replace("All", "Everyone")
-print(company_python)
+print(f"\nreplace the word all for veryone = {company_python}")
 
 """ 13
 Split the string 'Coding For All' using space 
 as the separator (split()) .
 """
-print(company.split())
+print(f"\nsplit coding for all = {company.split()}")
 
 """ 14
 "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon" 
 split the string at the comma.
 """
 companies = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
-print(companies.split(", "))
+print(f"\nVariable companies = {companies}")
+companies_split = companies.split(", ")
+print(f"\nVariable companies with comma = {companies_split}")
 
 """ 15
 What is the character at index 0 in the string Coding For All.
 """
-print(company[0])
+print(f"\nthe first character of Coding For All is = {company[0]}")
 
 """ 16
 What is the last index of the string Coding For All.
 """
-last_leter_company = len(company)-1
-print(company[last_leter_company])
-
-print(company[-1])  #otra forma de encontrar la ultima letra
+print(f"\nthe last character of Coding For All is  = {company[-1]}")
 
 """ 17
 What character is at index 10 in "Coding For All" string.
 """
+print(f"\nthe 10th character of Coding For All is  = {company[-1]}")
 print(company[10])
 
 """ 18
@@ -118,7 +120,7 @@ Primera = company_python_split[0][0]
 Segunda = company_python_split[1][0]
 Tercera = company_python_split[2][0]
 
-print(f"{Primera+Segunda+Tercera}")
+print(f"the acronym of Python For Everyone is = {Primera+Segunda+Tercera}")
 
 """ 19
 reate an acronym or an abbreviation for the name 
@@ -130,27 +132,30 @@ Primera = company_split[0][0]
 Segunda = company_split[1][0]
 Tercera = company_split[2][0]
 
-print(f"{Primera+Segunda+Tercera}")
+print(f"\nthe acronym of Coding For All is = {Primera+Segunda+Tercera}")
 
 """ 20
 Use index to determine the position of the first 
 occurrence of C in Coding For All.
 """
-print(company.find("C"))
+position_C = company.find("C")
+print(f"\nfirst ocurrence of C in Coding For All = {position_C}")
 
 """ 21
 Use index to determine the position of the first 
 occurrence of F in Coding For All.
 """
-print(company.find("F"))
+position_F = company.find("F")
+print(f"\nfirst ocurrence of F in Coding For All = {position_F}")
 
 """ 22
 Use rfind to determine the position of the last 
 occurrence of l in Coding For All People.
 """
 single_string_3 = company+" People"
+position_l = single_string_3.rfind("l")
 
-print(single_string_3.rfind("l"))
+print(f"\nlast occurrence of l in Coding For All People = {position_l}")
 
 """23
 Use index or find to find the position of the first 
@@ -159,21 +164,24 @@ occurrence of the word 'because' in the following sentence:
 conjunction'
 """
 single_string_4 = "You cannot end a sentence with because because because is a conjunction"
-
-print(single_string_4.find("because"))
+print(f"\noriginal text:\n{single_string_4}")
+position_because = single_string_4.find("because")
+print(f"\nfirst occurrence of because in the sentence = {position_because}")
 
 """ 24
 Use rindex to find the position of the last occurrence of 
 the word because in the following sentence: 'You cannot end a 
 sentence with because because because is a conjunction'
 """
-print(single_string_4.rfind("because"))
+position_because = single_string_4.rfind("because")
+print(f"\nlast occurrence of because in the sentence = {position_because}")
 
 """ 25
 Slice out the phrase 'because because because' in the 
 following sentence: 'You cannot end a sentence with because 
 because because is a conjunction'
 """
+print("\ntext whiuot because :")
 print(single_string_4[:31]+single_string_4[55:])   #imprime desde hasta, (concatena), desde hasta
 #dejarlos vacios significa [:31] = del inicio hasta 31 y [55:] = del 48 hasta el final
 
@@ -182,30 +190,35 @@ Find the position of the first occurrence of the word 'because'
 in the following sentence: 'You cannot end a sentence with because 
 because because is a conjunction'
 """
-print(single_string_4.index("because"))
+position_because = single_string_4.find("because")
+print(f"\nfirst occurrence of because in the sentence = {position_because}")
 
 """ 27
 Slice out the phrase 'because because because' in the following 
 sentence: 'You cannot end a sentence with because because because 
 is a conjunction'
 """
+print("\ntext whiuot because :")
 print(single_string_4[:31]+single_string_4[55:])
 
 """ 28
 Does ''Coding For All' start with a substring Coding?
 """
-print(company.startswith("Coding"))
+question_1 = company.startswith("Coding")
+print(f"\nDoes ''Coding For All' start with a substring Coding? R: {question_1}")
 
 """ 29
 Does 'Coding For All' end with a substring coding?
 """
-print(company.endswith("coding"))
+question_2 = company.endswith("coding")
+print(f"\nDoes ''Coding For All' end with a substring Coding? R: {question_2}")
 
 """ 30
 '   Coding For All      '  , remove the left and right 
 trailing spaces in the given string.
 """
 single_string_5 = '   Coding For All      '
+print("\nleft and right trailing spaces of '   Coding For All      '")
 print(single_string_5.strip())
 
 
@@ -215,7 +228,9 @@ method isidentifier():
     30DaysOfPython
     thirty_days_of_python
 """
+print("\n30DaysOfPython whit isidentifier is")
 print("30DaysOfPython".isidentifier())
+print("\nthirty_days_of_python with isidentifier is")
 print("thirty_days_of_python".isidentifier())
 
 """ 32
@@ -223,6 +238,8 @@ The following list contains the names of some of python libraries:
 ['Django', 'Flask', 'Bottle', 'Pyramid', 'Falcon']. Join the list 
 with a hash with space string.
 """
+print("\nlist: ['Django', 'Flask', 'Bottle', 'Pyramid', 'Falcon']")
+print("\nJoin the list with a hash with space string")
 print(" # ".join(['Django', 'Flask', 'Bottle', 'Pyramid', 'Falcon']))
 
 """ 33
@@ -230,14 +247,14 @@ Use the new line escape sequence to separate the following sentences.
     I am enjoying this challenge.
     I just wonder what is next.
 """
-print("I am enjoying this challenge.\nI just wonder what is next.")
+print("\nI am enjoying this challenge.\nI just wonder what is next.")
 
 """ 34
 Use a tab escape sequence to write the following lines.
     Name      Age     Country   City
     Asabeneh  250     Finland   Helsinki
 """
-print("Name     \tAge   \tCountry   \tCity")
+print("\nName     \tAge   \tCountry   \tCity")
 print("Asabeneh \t250   \tFinland   \tHelsinki")
 
 """ 35
@@ -248,7 +265,9 @@ Use the string formatting method to display the following:
 """
 radius = 10
 area = 3.14 * radius ** 2
-print(f"The area of a circle with radius {radius} is {area} meters square.")
+print(f"\nThe area of a circle with radius {radius} is {area} meters square.")
+
+print("The area of a circle with radius {} is {} meters square.".format(radius, area))
 
 """ 36
 Make the following using string formatting methods:
@@ -261,6 +280,6 @@ Make the following using string formatting methods:
     8 ** 6 = 262144
 """
 a, b = 8, 6
-print(f"{a} + {b} = {a+b}\n{a} - {b} = {a-b}\n{a} * {b} = {a*b}")
+print(f"\n{a} + {b} = {a+b}\n{a} - {b} = {a-b}\n{a} * {b} = {a*b}")
 print(f"{a} / {b} = {a/b}\n{a} % {b} = {a%b}\n{a} // {b} = {a//b}")
 print(f"{a} ** {b} = {a**b}")

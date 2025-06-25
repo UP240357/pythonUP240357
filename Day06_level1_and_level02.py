b65a41f25b9aa52d96c33bc306386c8140383bc4
UP240357 <up240357@alumnos.upa.edu.mx>
2025-06-25 11:28:50 -0600

    comienzo del lvl 2 del dia 6

--- Day06_level1_and_level02.py
+++ Day06_level1_and_level02.py
@@ -1,6 +1,8 @@
 # Day 6 lvl 1 y 2 - Tuples
 # Ruiz Clemente Jorge Alberto
 
+#lvl 1
+
 """ 1
 Create an empty tuple
 """
@@ -32,3 +34,38 @@ parents = ("Jorge","Norma")
 print(parents)
 familly = brothers+parents
 print(familly)
+
+#lvl 2
+
+""" 1
+Unpack siblings and parents from family_members
+"""
+
+""" 2
+Create fruits, vegetables and animal products tuples. Join the three tuples and assign it to a variable called food_stuff_tp.
+"""
+
+""" 3
+Change the about food_stuff_tp tuple to a food_stuff_lt list
+"""
+
+""" 4
+Slice out the middle item or items from the food_stuff_tp tuple or food_stuff_lt list.
+"""
+
+""" 5
+Slice out the first three items and the last three items from food_staff_lt list
+"""
+
+""" 6
+Delete the food_staff_tp tuple completely
+"""
+
+""" 7
+Check if an item exists in tuple:
+Check if 'Estonia' is a nordic country
+
+Check if 'Iceland' is a nordic country
+
+nordic_countries = ('Denmark', 'Finland','Iceland', 'Norway', 'Sweden')
+"""

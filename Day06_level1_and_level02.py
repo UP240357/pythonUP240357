9b6822a05b102e4c44ef1ef44e2b002eb330c8f0
UP240357 <up240357@alumnos.upa.edu.mx>
2025-06-27 10:45:25 -0600

    conclusion del dia 6. completado

--- Day06_level1_and_level02.py
+++ Day06_level1_and_level02.py
@@ -57,24 +57,33 @@ print(food_stuff_tp)
 """ 3
 Change the about food_stuff_tp tuple to a food_stuff_lt list
 """
+food_stuff_lt = list(food_stuff_tp)
+print(food_stuff_lt)
 
 """ 4
 Slice out the middle item or items from the food_stuff_tp tuple or food_stuff_lt list.
 """
+middle = len(food_stuff_lt)//2
+print(food_stuff_lt[middle])
 
 """ 5
 Slice out the first three items and the last three items from food_staff_lt list
 """
+print(f"{food_stuff_lt[0]}, {food_stuff_lt[1]}, {food_stuff_lt[3]}")
 
 """ 6
 Delete the food_staff_tp tuple completely
 """
+del food_stuff_tp
 
 """ 7
 Check if an item exists in tuple:
 Check if 'Estonia' is a nordic country
 
 Check if 'Iceland' is a nordic country
-
-nordic_countries = ('Denmark', 'Finland','Iceland', 'Norway', 'Sweden')
 """
+nordic_countries = ('Denmark', 'Finland','Iceland', 'Norway', 'Sweden')
+check = "Estonia" in nordic_countries
+print(check)
+check = "Iceland" in nordic_countries
+print(check)

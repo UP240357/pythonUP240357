195253b5d10900683f7aca47376cc752efd93b6f
UP240357 <up240357@alumnos.upa.edu.mx>
2025-07-18 10:52:54 -0600

    fin del dia 11 lvl2

--- Day11_level02.py
+++ Day11_level02.py
@@ -45,8 +45,9 @@ print(is_empty([]))
 
 """ 3
 Write different functions which take lists. 
-They should calculate_mean, calculate_median, calculate_mode, calculate_range, calculate_variance, calculate_std (standard deviation)."""
-print()
+They should calculate_mean, calculate_median, 
+calculate_mode, calculate_range, calculate_variance, 
+calculate_std (standard deviation)."""
 def calculate_mean(list):
     total = 0
     for number in list:
@@ -54,4 +55,70 @@ def calculate_mean(list):
     mean = total/(len(list))
     return mean
 
-#def calculate_median(list):
+def calculate_median(list):
+    sorted_list = sorted(list)
+    n = len(sorted_list)
+    if n % 2 == 1:
+        median = sorted_list[n//2]
+    else:
+        mid1 = sorted_list[n//2-1]
+        mid2 = sorted_list[n//2]
+        median = (mid1+mid2)/2
+    return median
+
+def calculate_mode(list):
+    counts = {}
+    if not list:
+        return "Error: La lista no puede estar vacía para calcular la moda."
+    for item in list:
+        counts[item] = counts.get(item,0)+1
+    max_count = 0
+    if counts:
+        max_count = max(counts.values())
+    modes = []
+    for item, count in counts.items():
+        if count == max_count:
+            modes.append(item)
+    if max_count == 1 and len(modes) == len(list):
+        return "Fashion doesn't exist"
+    return modes
+
+def calculate_range(list):
+    min_val = list[0]
+    max_val = list[0]
+    for number in list:
+        if number < min_val:
+            min_val = number
+        if number > max_val:
+            max_val = number
+    range_val = max_val - min_val
+    return range_val
+
+def calculate_variance(list):
+    mean_val = calculate_mean(list)
+    if isinstance(mean_val, str):
+        return mean_val
+    squared_dif_sum = 0
+    for number in list:
+        squared_dif_sum += (number - mean_val)**2
+    variance = squared_dif_sum/(len(list))
+    return variance
+
+def calculate_std(list):
+    variance = calculate_variance(list)
+    if isinstance(variance,str):
+        return variance
+    std_dev = variance**0.5
+    return std_dev
+
+print(f"\nmean of [1,2,3,4,5] is {calculate_mean([1,2,3,4,5])}")
+
+print(f"\nmedian of [1,5,2,4,3] is {calculate_median([1,5,2,4,3])}")
+
+print(f"\nmode of [1,5,2,4,3,5,5,6] is {calculate_mode([1,5,2,4,3,5,5,6])}")
+
+print(f"\nrenge of [1,5,2,4,3] is {calculate_range([1,5,2,4,3])}")
+
+print(f"\nvariance of [1,2,3,4,5] is {calculate_variance([1,2,3,4,5])}")
+
+print(f"\nstd of [1,2,3,4,5] is {calculate_std([1,2,3,4,5])}")
